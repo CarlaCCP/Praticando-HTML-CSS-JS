22b021f9cf3035dcb74c1db734ef09a9badd8f60
Carla Cristina <carldepaula230@gmail.com>
2020-11-26 22:38:27 -0300

    wip

--- Desafio- Landing Page/js/script.js
+++ Desafio- Landing Page/js/script.js
@@ -1,8 +1,6 @@
 let email = document.querySelector("#email")
-let assunto = document.querySelector("#assunto")
 let texto = document.querySelector("#texto")
 let emailOk = false
-let assuntoOk = false
 let textoOk = false
 
 email.style.width = "100%"
@@ -10,7 +8,8 @@ email.style.width = "100%"
 function validaEmail() {
     let txtEmail = document.querySelector("#txtEmail")
     if (email.value.indexOf('@') == -1 || email.value.indexOf(".") == -1) {
-        txtEmail.innerHTML = "Email Inválido"
+      txtEmail.innerHTML = "Email Inválido"
+        //alert("Email Invalido")
         txtEmail.style.color = "red"
     } else {
         txtEmail.innerHTML = "Email Válido"
@@ -20,19 +19,19 @@ function validaEmail() {
 }
 
 function validaTexto() {
-    let txtAssunto = document.querySelector("#txtTexto")
+    let txtTexto = document.querySelector("#txtTexto")
     if (texto.value.length >= 100) {
-        txtAssunto.innerHTML = "Texto muito grande, digite no maximo 100 caracteres"
-        txtAssunto.style.color = "red"
-        txtAssunto.style.display = "block"
+        txtTexto.innerHTML = "Texto muito grande, digite no maximo 100 caracteres"
+        txtTexto.style.color = "red"
+        txtTexto.style.display = "block"
     } else {
-        txtAssunto.style.display = "none"
-        assuntoOk = true
+        txtTexto.style.display = "none"
+        textoOk = true
     }
 }
 
 function enviar() {
-    if (emailOk == true && assuntoOk == true && textoOk == true) {
+    if (emailOk == true && textoOk == true) {
         alert("Formulário enviado")
     } else {
         alert("Preencha o formulário corretamente")
